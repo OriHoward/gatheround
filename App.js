559a74ab5d5877c0f1b91eb7d7cb4d9751eb36b8
OriHoward <ori.howard@gmail.com>
2022-11-09 19:52:14 +0200

    new login screen design

--- App.js
+++ App.js
@@ -1,75 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text,KeyboardAvoidingView,TouchableOpacity, TextInput ,ImageBackground,SafeAreaView,Alert} from 'react-native';
+import React from 'react';
+import { StyleSheet, Text ,SafeAreaView} from 'react-native';
+import SignInScreen from './app/screens/SignInScreen';
 
 
-export default function App() {
+
+
+const App = () => {
   return (
-      
-      <ImageBackground source={require("./Images/test.png")} resizeMode = "cover"
-       style = {styles.image}>
-        <SafeAreaView style={styles.container}>
-          <KeyboardAvoidingView style = {styles.container}>
-        <TouchableOpacity onPress={() => Alert.alert('Login button pressed')} style = {styles.button}>
-          <Text style = {styles.buttonText}> Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => Alert.alert('Create Account Button pressed')} style = {styles.buttonReg}>
-          <Text style = {styles.buttonText}> Create Account</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style = {styles.forgotBtn}>Forgot Password</Text>
-        </TouchableOpacity>
-        <TextInput style = {styles.input}  placeholder = "Username"/>
-        <TextInput style = {styles.input}  placeholder = "Password"/>
-      <StatusBar style="auto" />
-      </KeyboardAvoidingView>
+      <SafeAreaView style = {styles.container}>
+        <SignInScreen/>
+        <Text></Text>
       </SafeAreaView>
-      </ImageBackground>
       
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems : "center",
-    justifyContent : "flex-end"
-
-  },
-    image: {
-    flex: 1,
+    container: {
+      flex : 1,
     },
-    button : {
-      backgroundColor: "#72A2FE",
-      padding : 20,
-      borderRadius: 10,
-      width : 200,
-      height : 60,
-      top : 0
-    },
-    buttonReg : {
-      backgroundColor: "#72A2FE",
-      padding : 20,
-      borderRadius: 10,
-      width : 200,
-      height : 60,
-      top : 10
-    },
-    buttonText : {
-      textAlign : "center",
-      color : "black"
-    },
-    input: {
-      height: 50,
-      width : 220,
-      borderColor : "#383839",
-      textAlign : "center",
-      margin: 12,
-      borderWidth: 2,
-      padding: 7,
-      bottom : 400,
-    },
-    forgotBtn : {
-      height: 30,
-      margin : 20,
-    }
   });
+
+  export default App;
---
+++ app/screens/SignInScreen/SignInScreen.js
@@ -0,0 +1,46 @@
+import React, {useState} from "react";
+import { View,Text,Image,StyleSheet, useWindowDimensions} from 'react-native';
+import Logo from '../../../images/GatheRound.png';
+import CustomInput from "../components/CustomInput";
+
+
+const SignInScreen = () => {
+    const [username,setUsername] = useState('');
+    const [password,setPassword] = useState('');
+    const {height} = useWindowDimensions();
+    return (
+        <View style = {styles.root}>
+            <Image
+            source = {Logo}
+            style = {[styles.logo, {height : height * 0.3}]}
+            resizeMode = "contain"
+            />
+            <CustomInput
+            placeholder= "Username"
+            value = {username}
+            setValue = {setUsername}
+            />
+            <CustomInput
+            placeholder="password"
+            value={password}
+            setValue = {setPassword}
+            secureTextEntry = {true}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    root : {
+        alignItems: "center",
+        padding: 20,
+        backgroundColor : "white",
+    },
+    logo : {
+        width : '70%',
+        maxWidth: 600,
+        maxHeight : 450,
+    },
+});
+
+export default SignInScreen
