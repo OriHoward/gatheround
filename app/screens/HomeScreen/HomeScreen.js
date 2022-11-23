28c925caea004fb31ad24eddec00343d90e2cdb5
adiy55 <adiyafe55@gmail.com>
2022-11-23 18:33:12 +0200

    Add BottomNavi + HomeScreen.js

---
+++ app/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+import BottomNavi from "../components/BottomNavi";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 24, alignSelf: "center" }}>Homepage</Text>
+      <BottomNavi  />
+    </View>
+  );
+};
+
+export default HomeScreen;
--- app/screens/HomeScreen/index.js
+++ app/screens/HomeScreen/index.js
@@ -1,12 +1,1 @@
-import { View, Text } from "react-native";
-import React from "react";
-
-const HomeScreen = () => {
-  return (
-    <View>
-      <Text style={{ fontSize: 24, alignSelf: "center" }}>Homepage</Text>
-    </View>
-  );
-};
-
-export default HomeScreen;
+export { default } from "./HomeScreen";
