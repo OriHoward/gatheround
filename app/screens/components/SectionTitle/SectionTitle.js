3546d9a065d1caa0cae1ef1af15427f6a73b7c52
adiy55 <adiyafe55@gmail.com>
2022-12-08 22:51:23 +0200

    Add Section Title component

--- app/screens/components/SectionTitle/SectionTitle.js
+++ app/screens/components/SectionTitle/SectionTitle.js
@@ -1,13 +1,26 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const SectionTitle = ({ title }) => {
   return (
-    <View>
+    <View
+      style={{
+        alignItems: "center",
+        flexDirection: "row",
+        maxWidth: 500,
+      }}
+    >
       <Text
-        style={{ fontSize: 16, color: "gray", fontWeight: "bold", padding: 15 }}
+        style={{
+          textAlign: "center",
+          fontSize: 16,
+          color: "gray",
+          fontWeight: "bold",
+          padding: 15,
+        }}
       >
-        title
+        {title}
       </Text>
     </View>
   );
