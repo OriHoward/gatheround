22246a731b5576f629b4276d9d23698b7887cd46
OriHoward <ori.howard@gmail.com>
2022-12-14 13:30:17 +0200

    Added navigation to SignUpBusiness

--- app/screens/loginScreens/LoginContainer.js
+++ app/screens/loginScreens/LoginContainer.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 // Screens
 import SignInScreen from "./SignInScreen";
 import SignUpScreen from "./SignUpScreen";
+import SignUpScreenBusiness from "./SignUpScreen/SignUpBusiness";
 import ConfirmEmailScreen from "./ConfirmEmailScreen";
 import ForgotPasswordScreen from "./ForgotPasswordScreen";
 import NewPasswordScreen from "./NewPasswordScreen";
@@ -15,6 +16,7 @@ const LoginContainer = () => {
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="SignIn" component={SignInScreen} />
       <Stack.Screen name="SignUp" component={SignUpScreen} />
+      <Stack.Screen name="SignUpBusiness" component={SignUpScreenBusiness} />
       <Stack.Screen name="ConfirmEmail" component={ConfirmEmailScreen} />
       <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
       <Stack.Screen name="NewPassword" component={NewPasswordScreen} />
