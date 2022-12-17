cebacf0a73d9f7291718cfe245a378a80b364e6e
EitanKats <eitan.kats96@gmail.com>
2022-12-17 20:39:57 +0200

    added minimal search implementation

--- app/screens/mainScreens/SearchScreen/SearchScreen.js
+++ app/screens/mainScreens/SearchScreen/SearchScreen.js
@@ -1,27 +1,50 @@
-import { StyleSheet, View, Text } from "react-native";
-import React from "react";
-import { Searchbar } from "react-native-paper";
+import { StyleSheet, View, Text } from 'react-native'
+import React, { useContext, useState } from 'react'
+import { Searchbar } from 'react-native-paper'
+import CustomButton from '../../components/CustomButton'
+import { AxiosContext } from '../../../context/AxiosContext'
 
 const SearchScreen = () => {
-  const [searchQuery, setSearchQuery] = React.useState("");
+	const [searchQuery, setSearchQuery] = useState('')
+	const [dataToDisplay, setDataDataToDisplay] = useState([])
+	const { authAxios } = useContext(AxiosContext)
 
-  const onChangeSearch = (query) => setSearchQuery(query);
-  return (
-    <View style={styles.root}>
-      <Searchbar
-        placeholder="Search"
-        onChangeText={onChangeSearch}
-        value={searchQuery}
-      />
-    </View>
-  );
-};
+	const onChangeSearch = (query) => setSearchQuery(query)
+
+	const onSearchClick = async () => {
+		const resp = await authAxios.get(`/business-search?profession=${searchQuery}`)
+		const { data } = resp
+		const { results } = data
+		setDataDataToDisplay(results)
+	}
+	return (
+		<View>
+			<View style={styles.root}>
+				<Searchbar placeholder="Search profession" onChangeText={onChangeSearch} value={searchQuery} />
+				<CustomButton onPress={onSearchClick} text="Search"></CustomButton>
+			</View>
+			<View>
+				{dataToDisplay.map((entry) => {
+					const { id, profession, country, city, phone_number } = entry
+					return (
+						<View key={id}>
+							<Text>{profession}</Text>
+							<Text>{country}</Text>
+							<Text>{city}</Text>
+							<Text>{phone_number}</Text>
+						</View>
+					)
+				})}
+			</View>
+		</View>
+	)
+}
 const styles = StyleSheet.create({
-  root: {
-    alignItems: "center",
-    padding: 10,
-    maxWidth: 350,
-  },
-});
+	root: {
+		alignItems: 'center',
+		padding: 10,
+		maxWidth: 350,
+	},
+})
 
-export default SearchScreen;
+export default SearchScreen
