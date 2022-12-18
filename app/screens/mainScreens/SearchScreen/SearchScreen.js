86841b089b9b6e5e2ed87d602b778638946de853
adiy55 <adiyafe55@gmail.com>
2022-12-18 16:13:48 +0200

    Edit search screen

--- app/screens/mainScreens/SearchScreen/SearchScreen.js
+++ app/screens/mainScreens/SearchScreen/SearchScreen.js
@@ -1,55 +1,65 @@
-import { StyleSheet, View, Text } from 'react-native'
-import React, { useContext, useState } from 'react'
-import { Searchbar } from 'react-native-paper'
-import CustomButton from '../../components/CustomButton'
-import { AxiosContext } from '../../../context/AxiosContext'
+import { StyleSheet, View, Text } from "react-native";
+import React, { useContext, useState } from "react";
+import { Searchbar, Button } from "react-native-paper";
+import CustomButton from "../../components/CustomButton";
+import { AxiosContext } from "../../../context/AxiosContext";
 
 const SearchScreen = () => {
-	const [searchQuery, setSearchQuery] = useState('')
-	const [dataToDisplay, setDataDataToDisplay] = useState([])
-	const { authAxios } = useContext(AxiosContext)
+  const [searchQuery, setSearchQuery] = useState("");
+  const [dataToDisplay, setDataToDispay] = useState([]);
+  const { authAxios } = useContext(AxiosContext);
 
-	const onChangeSearch = (query) => setSearchQuery(query)
+  const onChangeSearch = (query) => setSearchQuery(query);
 
-	const onSearchClick = async () => {
-		try {
-			const resp = await authAxios.get(`/business-search?profession=${searchQuery}`)
-			const { data } = resp
-			const { results = [] } = data
-			setDataDataToDisplay(results)
-		} catch (error) {
-			console.error(error)
-			setDataDataToDisplay([])
-		}
-	}
-	return (
-		<View>
-			<View style={styles.root}>
-				<Searchbar placeholder="Search profession" onChangeText={onChangeSearch} value={searchQuery} />
-				<CustomButton onPress={onSearchClick} text="Search"></CustomButton>
-			</View>
-			<View>
-				{dataToDisplay.map((entry) => {
-					const { id, profession, country, city, phone_number } = entry
-					return (
-						<View key={id}>
-							<Text>{profession}</Text>
-							<Text>{country}</Text>
-							<Text>{city}</Text>
-							<Text>{phone_number}</Text>
-						</View>
-					)
-				})}
-			</View>
-		</View>
-	)
-}
+  const onSearchClick = async () => {
+    try {
+      const resp = await authAxios.get(
+        `/business-search?profession=${searchQuery}`
+      );
+      const { data } = resp;
+      const { results = [] } = data;
+      setDataToDispay(results);
+    } catch (error) {
+      console.error(error);
+      setDataToDispay([]);
+    }
+  };
+  return (
+    <View style={styles.root}>
+      <View style={styles.root}>
+        <Searchbar
+          onIconPress={onSearchClick}
+          placeholder="Search profession"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+        />
+        <Button color="black" uppercase={false} onPress={() => setDataToDispay([])}>
+          Clear
+        </Button>
+      </View>
+      <View>
+        {dataToDisplay.map((entry) => {
+          const { id, profession, country, city, phone_number } = entry;
+          return (
+            <View key={id} style={{ alignItems: "center" }}>
+              <Text>{profession}</Text>
+              <Text>{country}</Text>
+              <Text>{city}</Text>
+              <Text>{phone_number}</Text>
+            </View>
+          );
+        })}
+      </View>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-	root: {
-		alignItems: 'center',
-		padding: 10,
-		maxWidth: 350,
-	},
-})
+  root: {
+    alignSelf: "center",
+    alignItems: "center",
+    padding: 10,
+    maxWidth: 350,
+  },
+});
 
-export default SearchScreen
+export default SearchScreen;
