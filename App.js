5c7ffb01b4bd9021d314433ca1912429147d0479
OriHoward <ori.howard@gmail.com>
2022-12-19 00:17:11 +0200

    Added ignore warnings in expo app

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { StyleSheet, SafeAreaView } from "react-native";
 import Navigation from "./app/navigation";
+import { LogBox } from 'react-native';
+
+
+
 
 const App = () => {
+  LogBox.ignoreAllLogs(true)
   return (
     <SafeAreaView style={styles.container}>
       <Navigation />
