1fd1a60724e0b972badd52231cfca7d989b1722f
oriHoward <ori.howard@gmail.com>
2022-12-27 19:03:32 +0200

    Added new regex and changed var to let

--- app/utils/input-validation.js
+++ app/utils/input-validation.js
@@ -1,10 +1,10 @@
 export function isValidEmail(emailInput) {
-  var validRegex = /^[a-zA-Z0-9.]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  let validRegex = /^[a-zA-Z0-9.]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   return emailInput.match(validRegex) && emailInput.length > 0;
 }
 
 export function isValidPassword(passwordInput) {
-  var validRegex = "^(?=.*[0-9])(?=.*[a-z]).{5,20}$";
+  let validRegex = "^(?=.*[0-9])(?=.*[a-z]).{5,20}$";
   return passwordInput.match(validRegex) && passwordInput.length > 0;
 }
 
@@ -13,17 +13,22 @@ export function isIdentical(passwordInput, repeatPasswordInput) {
 }
 
 export function isValidStr(strInput) {
-  var validRegex = "^[a-zA-Z ]+$";
+  let validRegex = "^[a-zA-Z ]+$";
   return strInput.match(validRegex) && strInput.length > 0;
 }
 
 export function isValidNumber(number) {
-  var validRegex = /^\+?\d{10,13}$/;
+  let validRegex = /^\+?\d{10,13}$/;
+  return number.match(validRegex) && number.length > 0;
+}
+
+export function isNumber(number) {
+  let validRegex = /\d/;
   return number.match(validRegex) && number.length > 0;
 }
 
 export function checkCurrency(currency) {
-  var validRegex = /^[A-Z]{3}$/;
+  let validRegex = /^[A-Z]{3}$/;
   return currency.match(validRegex) && currency.length > 0;
 }
 
