e5e1e26974560f490a67630d8bc0717909ebfe9b
adiy55 <adiyafe55@gmail.com>
2023-01-12 16:14:07 +0200

    Add category icons file

---
+++ app/utils/category-icons.js
@@ -0,0 +1,6 @@
+export const categoryIcons = {
+  Wedding: { icon: "ring", color: "gold" },
+  Birthday: { icon: "party-popper", color: "limegreen" },
+  Reunion: { icon: "handshake", color: "dodgerblue" },
+  Anniversary: { icon: "glass-cocktail", color: "red" },
+};
