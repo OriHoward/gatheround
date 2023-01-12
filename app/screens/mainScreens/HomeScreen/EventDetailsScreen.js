017d97e39e5fae1f0cd646ce5bb07574ba49f21f
adiy55 <adiyafe55@gmail.com>
2023-01-12 16:14:49 +0200

    Add icon + put request

--- app/screens/mainScreens/HomeScreen/EventDetailsScreen.js
+++ app/screens/mainScreens/HomeScreen/EventDetailsScreen.js
@@ -11,20 +11,39 @@ import {
   Dialog,
   Portal,
   Provider,
+  Avatar,
 } from "react-native-paper";
 import SectionTitle from "../../components/SectionTitle";
 import { AxiosContext } from "../../../context/AxiosContext";
 import { CardStyles } from "../../../CommonStyles";
 import backgroundImage from "../../../../assets/Images/app-background.jpg";
+import { categoryIcons } from "../../../utils/category-icons";
 
 const EventDetailsScreen = ({ route, navigation }) => {
   const { authAxios } = useContext(AxiosContext);
-  const { id, name, event_date, address, description, limit_attending } =
-    route.params;
+  const {
+    id,
+    name,
+    event_date,
+    category,
+    address,
+    description,
+    limit_attending,
+  } = route.params;
   const [date, time] = event_date.split(" ");
 
   const [isSaved, setIsSaved] = useState(true);
   const [isDialogVisible, setDialogVisible] = useState(false);
+  const [data, setData] = useState({
+    id,
+    name,
+    date,
+    time,
+    category,
+    address,
+    description,
+    limit_attending,
+  });
 
   const peachColor = "#FF7F50";
 
@@ -32,12 +51,27 @@ const EventDetailsScreen = ({ route, navigation }) => {
   const hideDialog = () => setDialogVisible(false);
 
   const deleteEvent = async () => {
-    // todo: send to back
     try {
       const response = await authAxios.delete(`/events/${id}`);
       console.log(`delete response ${response.status}`);
       hideDialog();
       navigation.navigate("Home");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const putEventInfo = async () => {
+    try {
+      const formattedDate = `${date} ${time}`;
+      const dataToSend = {
+        ...data,
+        eventDate: formattedDate,
+      };
+      const response = await authAxios.put("/events", dataToSend);
+      if (response.status === 200) {
+        navigation.navigate("Home");
+      }
     } catch (error) {
       console.error(error);
     }
@@ -52,15 +86,25 @@ const EventDetailsScreen = ({ route, navigation }) => {
       >
         <Card mode="outlined" style={CardStyles.cardContainer}>
           <Card.Content>
-            <Title style={CardStyles.boldText}>{name}</Title>
-            <Text style={CardStyles.boldText}>{date}</Text>
-            <Text style={CardStyles.normalText}>{time}</Text>
-            <Text style={CardStyles.normalText}>{address}</Text>
+            <Title style={CardStyles.boldText}>{data.name}</Title>
+            <Text style={CardStyles.boldText}>{data.date}</Text>
+            <Text style={CardStyles.normalText}>{data.time}</Text>
+            <Text style={CardStyles.normalText}>{data.address}</Text>
             <Text> </Text>
             <Divider />
             <SectionTitle title={"Invitation Details"} />
-            <Paragraph>{description}</Paragraph>
+            <Paragraph>{data.description}</Paragraph>
           </Card.Content>
+          <Avatar.Icon
+            icon={categoryIcons[`${data.category}`].icon}
+            color={"white"}
+            size={50}
+            style={{
+              marginTop: 20,
+              alignSelf: "center",
+              backgroundColor: categoryIcons[`${data.category}`].color,
+            }}
+          />
           <Card.Actions>
             <Button
               icon="pencil"
@@ -86,31 +130,34 @@ const EventDetailsScreen = ({ route, navigation }) => {
             <Title style={CardStyles.boldText}>{"Edit Event Details"}</Title>
             <TextInput
               label={"Event Name"}
-              value={name}
+              value={data.name}
               activeUnderlineColor={peachColor}
+              onChangeText={(text) => setData({ ...data, name: text })}
             />
             <TextInput
               label={"Date"}
-              value={date}
+              value={data.date}
               activeUnderlineColor={peachColor}
             />
             <TextInput
               label={"Time"}
-              value={time}
+              value={data.time}
               activeUnderlineColor={peachColor}
             />
             <TextInput
               label={"Address"}
-              value={address}
+              value={data.address}
               activeUnderlineColor={peachColor}
+              onChangeText={(text) => setData({ ...data, address: text })}
             />
             <Text> </Text>
             <Divider />
             <SectionTitle title={"Invitation Details"} />
             <TextInput
               label={"Description"}
-              value={description}
+              value={data.description}
               activeUnderlineColor={peachColor}
+              onChangeText={(text) => setData({ ...data, description: text })}
             />
           </Card.Content>
           <Card.Actions>
@@ -118,7 +165,11 @@ const EventDetailsScreen = ({ route, navigation }) => {
               icon="content-save-edit"
               uppercase={false}
               color="black"
-              onPress={() => setIsSaved(true)}
+              onPress={() =>
+                putEventInfo()
+                  .then(() => setIsSaved(true))
+                  .catch((e) => console.error(e))
+              }
             >
               Save
             </Button>
