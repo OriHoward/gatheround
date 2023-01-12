77178de58b82a6ca0bf826fa9b28fc61440cf23c
adiy55 <adiyafe55@gmail.com>
2023-01-12 16:19:41 +0200

    Add background to archive screen

--- app/screens/mainScreens/ArchiveScreen/ArchiveScreen.js
+++ app/screens/mainScreens/ArchiveScreen/ArchiveScreen.js
@@ -1,10 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
 import React from "react";
+import backgroundImage from "../../../../assets/Images/app-background.jpg";
 
 const ArchiveScreen = () => {
   return (
-    <View>
-      <Text>ArchiveScreen</Text>
+    <View style={{ alignContent: "center", flex: 1 }}>
+      <ImageBackground
+        source={backgroundImage}
+        style={{ flex: 1 }}
+        imageStyle={{ opacity: 0.4 }}
+      >
+        <Text>ArchiveScreen</Text>
+      </ImageBackground>
     </View>
   );
 };
