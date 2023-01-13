fd7a051f4d5d2f7d28cd67af0afaae1850da811b
oriHoward <ori.howard@gmail.com>
2023-01-13 13:25:43 +0200

    Created new page BusinessRequestsScreen + get request to backend

---
+++ app/screens/mainScreens/BusinessRequestsScreen/BusinessRequestsScreen.js
@@ -0,0 +1,183 @@
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import React, { useState, useEffect, useContext } from "react";
+import { AxiosContext } from "../../../context/AxiosContext";
+import { Card, Button, Avatar } from "react-native-paper";
+import { categoryIcons } from "../../../utils/category-icons";
+
+const BusinessRequestsScreen = ({ navigation }) => {
+  const { authAxios } = useContext(AxiosContext);
+  const [requestsList, setRequestsList] = useState([]);
+
+  const getRequests = async () => {
+    try {
+      const response = await authAxios.get("/requests");
+      const { requests = [] } = response.data;
+      setRequestsList(requests);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleConfirm = () => {
+    console.log("confirm");
+  };
+  const handleDecline = () => {
+    console.log("decline");
+  };
+
+  useEffect(() => {
+    getRequests()
+      .then()
+      .catch((e) => console.error(e));
+  }, []);
+
+  const renderItems = ({ item }) => {
+    const {
+      id,
+      package_id,
+      event_id,
+      description,
+      request_status,
+      event_name,
+      event_date,
+      event_category,
+      event_address,
+    } = item;
+    return (
+      <View>
+        <Card style={styles.cardContainer}>
+          <Card.Content>
+            <View style={styles.cardHeader}>
+              <Text style={styles.cardTitle}>{event_name}</Text>
+              <Text style={styles.cardSubtitle}>{event_category}</Text>
+              <Card.Title
+                left={(props) => (
+                  <Avatar.Icon
+                    {...props}
+                    icon={categoryIcons[`${event_category}`].icon}
+                    color={"white"}
+                    style={{
+                      backgroundColor: categoryIcons[`${event_category}`].color,
+                      marginTop: -35,
+                    }}
+                  />
+                )}
+              />
+            </View>
+            <View>
+              <View style={styles.hostMessageContainer}>
+                <Text style={styles.hostMessageTitle}>Host Message:</Text>
+                <Text style={styles.hostMessage}>{description}</Text>
+              </View>
+              <View style={styles.cardDetailContainer}>
+                <View style={styles.cardDetail}>
+                  <Text style={styles.cardDetailLabel}>Date:</Text>
+                  <Text style={styles.cardDetailText}>{event_date}</Text>
+                </View>
+                <View style={styles.cardDetail}>
+                  <Text style={styles.cardDetailLabel}>Address:</Text>
+                  <Text style={styles.cardDetailText}>{event_address}</Text>
+                </View>
+              </View>
+            </View>
+          </Card.Content>
+          <Card.Actions style={styles.cardActions}>
+            <Button
+              icon="check"
+              color={"green"}
+              onPress={() => handleConfirm(id)}
+            >
+              Confirm
+            </Button>
+            <Button
+              icon={"cancel"}
+              color={"red"}
+              onPress={() => handleDecline(id)}
+            >
+              Decline
+            </Button>
+          </Card.Actions>
+        </Card>
+      </View>
+    );
+  };
+
+  return (
+    <View>
+      <FlatList
+        style={{ width: "100%" }}
+        data={requestsList}
+        renderItem={renderItems}
+        keyExtractor={(item) => item.id}
+      ></FlatList>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    alignSelf: "center",
+  },
+  cardContainer: {
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    elevation: 5,
+    minWidth: "50%",
+  },
+  cardHeader: {
+    flexDirection: "row",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  cardTitle: {
+    fontWeight: "bold",
+    fontSize: 20,
+    marginTop: 5,
+  },
+  cardSubtitle: {
+    fontSize: 18,
+    color: "#666",
+    marginTop: 6,
+  },
+  hostMessageContainer: {
+    marginBottom: 10,
+    marginTop: -25,
+  },
+  hostMessageTitle: {
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  hostMessage: {
+    fontSize: 14,
+    color: "#666",
+    fontWeight: "bold",
+    paddingVertical: 5,
+  },
+
+  cardDetailContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  cardDetail: {
+    flex: 1,
+    marginRight: 10,
+    fontWeight: "bold",
+  },
+  cardDetailLabel: {
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  cardDetailText: {
+    fontWeight: "bold",
+    fontSize: 14,
+    color: "#666",
+  },
+  cardActions: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+});
+
+export default BusinessRequestsScreen;
