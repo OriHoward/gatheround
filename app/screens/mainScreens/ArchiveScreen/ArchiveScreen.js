dcc56c443590061fa9d68eb03d3436e049f36c9d
adiy55 <adiyafe55@gmail.com>
2023-01-13 16:10:16 +0200

    WIP archive screen, add get request

--- app/screens/mainScreens/ArchiveScreen/ArchiveScreen.js
+++ app/screens/mainScreens/ArchiveScreen/ArchiveScreen.js
@@ -1,8 +1,81 @@
-import { View, Text, ImageBackground } from "react-native";
-import React from "react";
+import { View, ImageBackground, SectionList } from "react-native";
+import React, { useState, useContext } from "react";
+import { AxiosContext } from "../../../context/AxiosContext";
 import backgroundImage from "../../../../assets/Images/app-background.jpg";
+import {
+  Card,
+  Button,
+  Avatar,
+  Text,
+  ActivityIndicator,
+} from "react-native-paper";
+import { CardStyles } from "../../../CommonStyles";
+import SectionTitle from "../../components/SectionTitle";
 
 const ArchiveScreen = () => {
+  const { authAxios } = useContext(AxiosContext);
+  const [pastEvents, setPastEvents] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  const getPastEvents = async () => {
+    try {
+      const response = await authAxios.get("/archive");
+      const { data } = response;
+      const { past_events = [] } = data;
+      if (past_events.length === 0) {
+        setPastEvents([
+          {
+            title: "No Past Events",
+            data: past_events,
+          },
+        ]);
+      } else {
+        setPastEvents([
+          {
+            title: "Past Events",
+            data: past_events,
+          },
+        ]);
+      }
+    } catch (error) {
+      console.error(error);
+      setPastEvents([]);
+    }
+  };
+
+  const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
+
+  const renderItem = () => {
+    return (
+      <Card style={CardStyles.cardContainer}>
+        <Card.Title
+          title="Card Title"
+          subtitle="Card Subtitle"
+          left={LeftContent}
+        />
+        <Card.Content>
+          <Text variant="titleLarge">Card title</Text>
+          <Text variant="bodyMedium">Card content</Text>
+        </Card.Content>
+        <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
+        <Card.Actions>
+          <Button>Cancel</Button>
+          <Button>Ok</Button>
+        </Card.Actions>
+      </Card>
+    );
+  };
+
+  if (isLoading) {
+    getPastEvents()
+      .then(() => setLoading(false))
+      .catch((e) => console.error(e));
+    return (
+      <View style={{ padding: 20 }}>
+        <ActivityIndicator animating={true} />
+      </View>
+    );
+  }
   return (
     <View style={{ alignContent: "center", flex: 1 }}>
       <ImageBackground
@@ -10,7 +83,15 @@ const ArchiveScreen = () => {
         style={{ flex: 1 }}
         imageStyle={{ opacity: 0.4 }}
       >
-        <Text>ArchiveScreen</Text>
+        <SectionList
+          sections={pastEvents}
+          renderItem={renderItem}
+          renderSectionHeader={({ section: { title } }) => (
+            <SectionTitle title={title} />
+          )}
+          onRefresh={() => setLoading(true)}
+          refreshing={isLoading}
+        />
       </ImageBackground>
     </View>
   );
