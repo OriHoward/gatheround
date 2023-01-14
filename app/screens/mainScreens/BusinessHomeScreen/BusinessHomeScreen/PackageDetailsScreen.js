d403a2ee0100a7be713448173545607c4ac218a9
adiy55 <adiyafe55@gmail.com>
2023-01-14 17:26:44 +0200

    Fix BUG

--- app/screens/mainScreens/BusinessHomeScreen/BusinessHomeScreen/PackageDetailsScreen.js
+++ app/screens/mainScreens/BusinessHomeScreen/BusinessHomeScreen/PackageDetailsScreen.js
@@ -185,7 +185,6 @@ const PackageDetailsScreen = ({ route, navigation }) => {
 
 const styles = StyleSheet.create({
   root: {
-    // alignSelf: "strech",
     marginTop: 15,
     marginBottom: 15,
     flex: 1,
