06219a2a0931e173f36a856336b317bff659a06a
adiy55 <adiyafe55@gmail.com>
2023-01-14 19:25:37 +0200

    Display events + details for accepted events + display FAB over components

--- app/screens/mainScreens/BusinessCalendar/BusinessCalendarScreen/BusinessCalendarScreen.js
+++ app/screens/mainScreens/BusinessCalendar/BusinessCalendarScreen/BusinessCalendarScreen.js
@@ -1,10 +1,12 @@
 import { View, StyleSheet, FlatList } from "react-native";
 import React, { useState, useContext } from "react";
-import { Card, FAB, IconButton, Avatar } from "react-native-paper";
+import { Card, FAB, IconButton, Avatar, Text } from "react-native-paper";
 import { DatePickerModal } from "react-native-paper-dates";
 import { AxiosContext } from "../../../../context/AxiosContext";
 import { getBackendDateFormat } from "../../../../utils/datetime-utils";
 import { useFocusEffect } from "@react-navigation/native";
+import { categoryIcons } from "../../../../utils/category-icons";
+import { CardStyles, EventCardStyles } from "../../../../CommonStyles";
 
 const BusinessCalendarScreen = ({ navigation }) => {
   const { authAxios } = useContext(AxiosContext);
@@ -40,20 +42,59 @@ const BusinessCalendarScreen = ({ navigation }) => {
   const getCalendarEvents = async () => {
     try {
       const response = await authAxios.get("/booked-dates");
+      const calendarData = [];
       const unavailableDates = [];
       response.data.forEach((element) => {
         if (element.category === "Unavailable") {
           const [day, month, year] = element.date.split("/");
           const formattedDate = `${year}-${month}-${day}`;
           unavailableDates.push(new Date(formattedDate));
+          calendarData.push({
+            ...element,
+            dateObject: new Date(formattedDate),
+          });
         }
       });
       setDates(unavailableDates);
-      setData(response.data);
+
+      const acceptedEvents = await authAxios.get(
+        "/requests?accepted-only=True"
+      );
+      const { requests = [] } = acceptedEvents.data;
+      requests.forEach((element) => {
+        const {
+          id,
+          description,
+          event_name: name,
+          event_date,
+          event_category: category,
+          event_address: address,
+        } = element;
+        const [date, time] = event_date.split(" ");
+        const [day, month, year] = date.split("/");
+        const formattedDate = `${year}-${month}-${day}T${time}`;
+        calendarData.push({
+          id,
+          description,
+          name,
+          date,
+          time,
+          category,
+          address,
+          dateObject: new Date(formattedDate),
+        });
+      });
+
+      calendarData.sort((a, b) => {
+        return a.dateObject - b.dateObject;
+      });
+
+      setData(calendarData);
     } catch (e) {
       console.error(e);
     }
   };
+
   useFocusEffect(
     React.useCallback(() => {
       getCalendarEvents()
@@ -68,33 +109,81 @@ const BusinessCalendarScreen = ({ navigation }) => {
   );
 
   const renderItem = ({ item }) => {
-    const { id, date, category, description } = item;
-    return (
-      <Card
-        mode="outlined"
-        style={styles.cardContainer}
-        key={id}
-        onPress={() =>
-          navigation.navigate("Details", {
-            id,
-            date,
-            category,
-            description,
-          })
-        }
-      >
-        <Card.Title
-          title={date}
-          subtitle={category}
-          left={LeftContent}
-          right={RightContent}
-        />
-      </Card>
-    );
+    if (item.category === "Unavailable") {
+      const { id, date, category, description } = item;
+      return (
+        <Card
+          mode="outlined"
+          style={styles.cardContainer}
+          key={id}
+          onPress={() =>
+            navigation.navigate("Details", {
+              id,
+              date,
+              category,
+              description,
+            })
+          }
+        >
+          <Card.Title
+            title={date}
+            subtitle={category}
+            left={LeftContent}
+            right={RightContent}
+          />
+        </Card>
+      );
+    } else {
+      const { id, description, name, date, time, category, address } = item;
+      return (
+        <Card
+          style={CardStyles.cardContainer}
+          mode="outlined"
+          key={id}
+          onPress={() =>
+            navigation.navigate("Details", {
+              id,
+              description,
+              name,
+              date,
+              time,
+              category,
+              address,
+            })
+          }
+        >
+          <Card.Title
+            title={name}
+            subtitle={address}
+            left={(props) => (
+              <Avatar.Icon
+                {...props}
+                icon={categoryIcons[`${category}`]?.icon || "calendar-star"}
+                color={"white"}
+                style={{
+                  backgroundColor:
+                    categoryIcons[`${category}`]?.color || "blueviolet",
+                }}
+              />
+            )}
+            right={RightContent}
+          />
+          <Card.Content>
+            <View style={{ flexDirection: "row" }}>
+              <Text style={EventCardStyles.header2_date}>{date}</Text>
+              <Text style={EventCardStyles.header2_time}>{`, ${time}`}</Text>
+            </View>
+          </Card.Content>
+        </Card>
+      );
+    }
   };
 
   return (
     <View style={styles.root}>
+      <View>
+        <FlatList data={data} renderItem={renderItem} />
+      </View>
       <DatePickerModal
         visible={isDatePickerVisible}
         onDismiss={onDismiss}
@@ -119,9 +208,6 @@ const BusinessCalendarScreen = ({ navigation }) => {
         ]}
         onStateChange={({ open }) => setOpen(open)}
       />
-      <View>
-        <FlatList data={data} renderItem={renderItem} />
-      </View>
     </View>
   );
 };
